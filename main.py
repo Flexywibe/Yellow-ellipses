929af271d614c4bd1fd86c4b9e14cbcfc08cd5ba
Artyom Troyazykov <mikrobamboni@yandex.ru>
2021-04-13 17:39:32 +0700

    1

--- main.py
+++ main.py
@@ -1,43 +1,74 @@
-import sys
+from flask import Flask, request
+import logging
 
-from random import randrange
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QColor
+import json
 
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        uic.loadUi('main1.ui', self)
-        self.pushButton.clicked.connect(self.run)
-        self.do_paint = False
-
-    def run(self):
-        self.do_paint = True
-        self.repaint()
-
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.draw_ellipse(qp)
-            qp.end()
-
-    def draw_ellipse(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        radius = randrange(10, 190)
-        coords = randrange(800), randrange(500)
-        qp.drawEllipse(coords[0] - radius, coords[1] - radius, 2 * radius, 2 * radius)
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+sessionStorage = {}
 
 
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
+@app.route('/post', methods=['POST'])
+def main():
+    logging.info(f'Request: {request.json!r}')
+    response = {
+        'session': request.json['session'],
+        'version': request.json['version'],
+        'response': {
+            'end_session': False
+        }
+    }
+    handle_dialog(request.json, response)
+
+    logging.info(f'Response:  {response!r}')
+    return json.dumps(response)
+
+
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        sessionStorage[user_id] = {
+            'suggests': [
+                "Не хочу.",
+                "Не буду.",
+                "Отстань!",
+            ]
+        }
+        res['response']['text'] = 'Привет! Купи слона!'
+        res['response']['buttons'] = get_suggests(user_id)
+        return
+    if req['request']['original_utterance'].lower() in [
+        'ладно',
+        'куплю',
+        'покупаю',
+        'хорошо'
+    ]:
+        res['response']['text'] = 'Слона можно найти на Яндекс.Маркете!'
+        res['response']['end_session'] = True
+        return
+    res['response']['text'] = \
+        f"Все говорят '{req['request']['original_utterance']}', а ты купи слона!"
+    res['response']['buttons'] = get_suggests(user_id)
+
+
+def get_suggests(user_id):
+    session = sessionStorage[user_id]
+    suggests = [
+        {'title': suggest, 'hide': True}
+        for suggest in session['suggests'][:2]
+    ]
+    session['suggests'] = session['suggests'][1:]
+    sessionStorage[user_id] = session
+    if len(suggests) < 2:
+        suggests.append({
+            "title": "Ладно",
+            "url": "https://market.yandex.ru/search?text=слон",
+            "hide": True
+        })
+
+    return suggests
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MainWindow()
-    ex.show()
-    sys.excepthook = except_hook
-    sys.exit(app.exec())
+    app.run()
